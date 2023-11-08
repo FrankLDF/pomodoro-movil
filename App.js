fc632ad4ebccc73abac6a832c6157e6a9f2a52e4
FrankLDF <frankluisduarte00@gmail.com>
2023-11-08 13:36:22 -0400

    feat(header): create header atom component

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import { useState, useEffect } from "react";
+
+import Header from "./src/components/atoms/header";
+
+const colors = ["#F7DC6F", "#A2DADF", "#D7BDE2"];
 
 export default function App() {
+    const [isWorking, setIsWorking] = useState(false);
+    const [time, setTime] = useState(25 * 60);
+    const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "LONG");
   return (
-    <View style={styles.container}>
-      <Text>Vamos a iniciar a trabajar</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]}
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 15,
+          paddingTop: Platform.OS === "android" && 30,
+        }}
+      >
+        <Text style={styles.titleText}>Pomodoro</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#259",
+  },
+  titleText: {
+    marginTop: 5,
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#444",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+    alignItems: "center",
   },
 });
